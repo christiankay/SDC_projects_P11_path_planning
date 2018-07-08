21494b3da58777d0c465757fb534e06ea221ee04
christiankay <c0knaak@gmail.com>
2018-07-08 14:32:21 +0200

    bug1

--- src/main.cpp
+++ src/main.cpp
@@ -247,8 +247,8 @@ int main() {
             double dist_inc = 0.5; // space between point euqals approx. 50 mph
             for(int i = 0; i < 50; i++) // 50 point path planner
             {
-                  double next_s = car_s+(i+1)*dist_inc
-                  double next_d = 6
+                  double next_s = car_s+(i+1)*dist_inc;
+                  double next_d = 6;
                   std::vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
 
                   next_x_vals.push_back(xy[0]); // const velo , const angle
